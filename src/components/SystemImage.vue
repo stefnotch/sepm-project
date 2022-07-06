686ea80f9218b55d974cccc35198e7420854ac31
stefnotch <brandmairstefan@gmail.com>
2022-07-06 13:14:25 +0200

    Import project
    Without git history

---
+++ src/components/SystemImage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { findBackendUrl } from '@/services/service';
+
+defineProps<{ name: string }>();
+
+const imagesBackendUrl = findBackendUrl('api/v1/images');
+</script>
+<template>
+  <img :src="imagesBackendUrl + '/' + name" v-bind="$attrs" />
+</template>
